63602de105bf511e9726fe21f38f2ddf719cfc4d
miladhp <milad3hp@gmail.com>
2025-01-17 13:29:15 -0600

    add nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: true,
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxthq/studio'],
   $production: {
     routeRules: {
       '/**': { isr: true }

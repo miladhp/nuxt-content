3743cfed0457937d7cead074b3999e16d5779f69
miladhp <milad3hp@gmail.com>
2025-01-17 12:49:12 -0600

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/content'],
+  $production: {
+    routeRules: {
+      '/**': { isr: true }
+    }
+  },
+  $development: {
+    //
+  },
+  $env: {
+    staging: {
+      //
+    }
+  },
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3, // include h3 headings
+        },
+      }
+    }
+  }
+})
